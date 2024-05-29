9de7f842e841e6b13899e712d3bff3cdad174cbe
Administrator <simple852@naver.com>
2024-05-29 17:52:56 +0900

    좌석 선택 구현중

--- src/components/Ticketing.js
+++ src/components/Ticketing.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import '../css/Ticketing.css';
 import axios from 'axios';
+import seatsData from '../data/seatsData.json';
 
 // react-slick
 import Slider from 'react-slick';
@@ -27,8 +28,20 @@ function Ticketing() {
     const [moviePoster, setMoviePoster] = useState(null);
     const [selectedScreenTime, setSelectedScreenTime] = useState(null);
     const [selectedScreenHallName, setSelectedScreenHallName] = useState(null);
-    const [step, setStep] = useState(0);
+    const [seatMap, setSeatMap] = useState(null);
+    const [step, setStep] = useState(1);
+    const [adult, setAdult] = useState(0);
+    const [youth, setYouth] = useState(0);
 
+    // 성인 
+    const handleAdult = (count) => {
+        setAdult(count);
+    }
+
+    // 청소년
+    const handleYouth = (count) => {
+        setYouth(count);
+    }
 
     // 지역 선택 함수
     const handleRegion = (region) => {
@@ -77,6 +90,32 @@ function Ticketing() {
         setSelectedMovieTitle(title);
     }
 
+    // 다음 스텝 버튼 클릭 함수
+    const handleNextStepBtn = () => {
+        console.log(selectedMovieTitle);
+        if (!selectedMovieTitle) {
+            alert('영화를 선택해 주세요.');
+            return;
+        }
+
+        if (!selectedTheaterName) {
+            alert('극장을 선택해 주세요.');
+            return;
+        }
+
+        if (!selectedDate) {
+            alert('날짜를 선택해 주세요.');
+            return;
+        }
+
+        if (!selectedScreenTime) {
+            alert('상영시간을 선택해 주세요.');
+            return;
+        }
+
+        setStep(2);
+    }
+
     useEffect(() => {
         // 2024-05-28 :: 현재 상영중 영화 리스트 가져오기 (구현 예정)
         axios.get('/movie')
@@ -96,7 +135,6 @@ function Ticketing() {
                 console.log(err);
             })
 
-        
         // 오늘 기준 한달 뒤 날짜 리스트 생성
 
         // 오늘 날짜 
@@ -157,9 +195,8 @@ function Ticketing() {
         setRegionMap(regionMap);
     }, [theaterList])
 
-    // 지역 선택 시 selected class 추가 및 극장 버튼의 selected class 모두 제거
+    // 지역 선택 시 선택 요소에 selected class 추가 및 극장 버튼의 selected class 모두 제거
     useEffect(() => {
-        // 선택한 지역에 selected class 추가
         const regionBtns = document.querySelectorAll('.region-container button');
 
         regionBtns.forEach(btn => {
@@ -174,11 +211,12 @@ function Ticketing() {
         const theaterNameBtns = document.querySelectorAll('.theater-name-container button');
 
         theaterNameBtns.forEach(btn => {
-                btn.classList.remove('selected');
+            btn.classList.remove('selected');
+            setSelectedTheaterName(null);
         })
     }, [selectedRegion])
 
-    // 극장 선택 시 selected class 추가
+    // 극장 선택 시 선택 요소에 selected class 추가
     useEffect(() => {
         const theaterNameBtns = document.querySelectorAll('.theater-name-container button');
 
@@ -209,6 +247,7 @@ function Ticketing() {
         setDateMap(dateMap);
     }, [dateList, selectedDate]);
 
+    // 영화, 극장, 날짜 모두 선택 했을 때, 상영시간 및 좌석 정보 set
     useEffect(() => {
         if (selectedMovieTitle !== null && selectedDate !== null && selectedTheaterName !== null) {
             axios.get(`/theater/screenInfo/${selectedMovieTitle}/${selectedDate}/${selectedTheaterName}`)
@@ -221,6 +260,7 @@ function Ticketing() {
         }
     }, [selectedMovieTitle, selectedDate, selectedTheaterName])
 
+    // 상영관별 예매 된 좌석수를 가져오는 코드
     useEffect(() => {
         const createReservedSeatCountList = async () => {
             const reservedSeatCountList = [];
@@ -251,6 +291,7 @@ function Ticketing() {
         createReservedSeatCountList();
     }, [screenInfoList])
 
+    // 예매 된 좌석수를 뺀 후에 상영정보 set
     useEffect(() => {
         let idx = -1;
         if (screenInfoList.length === 0) {
@@ -262,34 +303,40 @@ function Ticketing() {
                 <div>
                     <p className='screen-hall-name'>{screenInfo.screen_hall_name}</p>
                     <div class='screen-time-container'>
-                        {screenInfo.time.split('|').map(time => {
-                            return (<button onClick={() => handleScreenTimeAndHall(time, screenInfo.screen_hall_name)} className={screenInfo.seat_count - reservedSeatCountList[++idx] === 0 ? 'full-reservation screen-time-btn' : 'screen-time-btn'}>
-                                <p className='screen-time'>{time}</p>
+                        {screenInfo.time.split('|').map(time =>
+                            // className 조건 두 개 이상 하는 법 -> {` `}
+                            <button onClick={() => handleScreenTimeAndHall(time, screenInfo.screen_hall_name)} className={`${screenInfo.seat_count - reservedSeatCountList[++idx] === 0 ? 'full-reservation screen-time-btn' : 'screen-time-btn'} ${time === selectedScreenTime ? 'selected' : ''}`}>
+                                <p>{time}</p>
                                 <p className={screenInfo.seat_count - reservedSeatCountList[idx] === 0 ? 'red' : ''}>{screenInfo.seat_count - reservedSeatCountList[idx] === 0 ? '매진' : screenInfo.seat_count - reservedSeatCountList[idx] + ' / ' + screenInfo.seat_count}</p>
-                            </button>)
-                        })}
+                            </button>
+                        )}
                     </div>
                 </div>
 
             )
             setScreenInfoMap(screenInfoMap);
         }
-    }, [reservedSeatCountList])
+    }, [reservedSeatCountList, selectedScreenTime])
 
+    // step 변경
     useEffect(() => {
-        const screenTimeBtns = document.querySelectorAll('.screen-time-btn');
-
-        screenTimeBtns.forEach(btn => {
-            const screenTime = btn.querySelector('.screen-time');
-
-            if (screenTime.innerText === selectedScreenTime) {
-                btn.classList.add('selected');
-            } else {
-                btn.classList.remove('selected');
+        if (step === 2) {
+            const theaterData = seatsData[selectedTheaterName];
+            if (theaterData) {
+                const seatMap = theaterData[selectedScreenHallName].map(row =>
+                    <div className="seat-row">
+                        {row.map(seat =>
+                            <div className="seat">
+                                {seat}
+                            </div>
+                        )}
+                    </div>
+                )
+                setSeatMap(seatMap);
             }
+        }
 
-        })
-    }, [selectedScreenTime])
+    }, [step])
 
     // slick setting
     const settings = {
@@ -300,29 +347,71 @@ function Ticketing() {
 
     return (
         <div id='ticketingMain' className='main'>
-            <div className='ticketing-option-container'>
-                <div className='section movie-section'>
-                    <ul>
-                        {movieMap}
-                    </ul>
-                </div>
-                <div className='section theater-section'>
-                    <div class='region-container'>
-                        {regionMap}
+            {
+                step === 1 ?
+                    <div className='ticketing-option-container'>
+                        <div className='section movie-section'>
+                            <ul>
+                                {movieMap}
+                            </ul>
+                        </div>
+                        <div className='section theater-section'>
+                            <div class='region-container'>
+                                {regionMap}
+                            </div>
+                            <div class='theater-name-container'>
+                                {theaterNameMap}
+                            </div>
+                        </div>
+                        <div className='section screen-info-section'>
+                            <Slider {...settings}>
+                                {dateMap}
+                            </Slider>
+                            <div className='screen-info-container'>
+                                {screenInfoMap}
+                            </div>
+                        </div>
                     </div>
-                    <div class='theater-name-container'>
-                        {theaterNameMap}
+                    : <div className='select-seat-container'>
+                        <div className='people'>
+                            <ul>
+                                <li>
+                                    <span className='people-type'>일반</span>
+                                    <span>
+                                        {
+                                            [0, 1, 2, 3, 4].map(count =>
+                                                <button className={adult === count ? 'selected people-count' : 'people-count'} onClick={() => handleAdult(count)}>{count}</button>
+                                            )
+                                        }
+                                    </span>
+                                </li>
+                                <li>
+                                    <span className='people-type'>청소년</span>
+                                    <span>
+                                        {
+                                            [0, 1, 2, 3, 4].map(count =>
+                                                <button className={youth === count ? 'selected people-count' : 'people-count'} onClick={() => handleYouth(count)}>{count}</button>
+                                            )
+                                        }
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="seat-info">
+                            <div className='seat-left'>
+                                <div className="screen">
+                                    스크린
+                                </div>
+                                <div className='seat-box'>
+                                    {seatMap}
+                                </div>
+                            </div>
+                            <div className='seat-right'></div>
+                        </div>
+
                     </div>
-                </div>
-                <div className='section screen-info-section'>
-                    <Slider {...settings}>
-                        {dateMap}
-                    </Slider>
-                    <div className='screen-info-container'>
-                        {screenInfoMap}
-                    </div>
-                </div>
-            </div>
+            }
             <div className='ticketing-info-container'>
                 <div className='movie'>
                     {moviePoster === null ? <p>영화선택</p> : <img className='poster' src={moviePoster} />}
@@ -340,20 +429,22 @@ function Ticketing() {
                         <span>상영관</span>
                         <span>{selectedScreenHallName}</span>
                     </div>
-                    <div className='info people'>
+                    <div className='info'>
                         <span>인원</span>
-                        <span></span>
+                        <span>{`일반 ${adult}명, 청소년 ${youth}명`}</span>
                     </div>
                 </div>
-                <div className="seat">
+                <div>
                     좌석 선택
                 </div>
                 <div className="payment">
                     결제
                 </div>
-                <button className="next-step-btn">
+                {step === 1 ? <button className="next-step-btn" onClick={handleNextStepBtn}>
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAtElEQVR4nO3YPQrCQBRF4VPpfiytLSyMC1BSGLNVK7GwcQFWgrV/hUYCU4hoIYGY+7gfvAUcZpiZBMzMzMzMOmgIbNKMELYDqjQ3IEPU/iVEOmYG3D/ETBC0+BIzRZBj1FYmQ5BjfpEDx7elb3MuwJiGesD5jxFVmmvTe6YPnCKE1ObAQX1rtamI8HQpItwlS0d0RBnhBVxGiMiBh/rpFOpTdxshojYAVsBa/XeQmZmZmaHlCeC06ncEGe4qAAAAAElFTkSuQmCC" />
+                    <p>좌석선택</p>
+                </button> : <button>스텝 2 버튼 만들 예정</button>}
 
-                </button>
             </div>
         </div>
     )
