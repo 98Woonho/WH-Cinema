b9da1e73ba6919a913d130eea39ee3af59fa208f
98Woonho <9woonho8@gmail.com>
2024-06-27 13:23:12 +0900

    .

--- express/routes/movieRoutes.js
+++ express/routes/movieRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db.js');
 
+
 router.get('/', (req, res) => {
     db.query(`SELECT * FROM movie`, (err, data) => {
         if (err) {
