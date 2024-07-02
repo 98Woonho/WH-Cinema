35d7c18829359d98e36b8bf78e1526c8f72b68f6
98Woonho <9woonho8@gmail.com>
2024-07-03 01:31:41 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-
-
-
-
-
 import { useState, useEffect } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -21,6 +16,8 @@ ${reset}
 `;
 
 function App() {
+  const location = useLocation();
+
   // axios.get('http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&ServiceKey=FPWJ81L14L7X38342790&releaseDts=20240627&ratedYn=y&releaseDte=20240727&listCount=500')
   //   .then(res => {
   //     console.log(res);
@@ -31,25 +28,10 @@ function App() {
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  // accessToken, refreshToken 검증
-  axios.get('/user/verify', { withCredentials: true })
-    .then(res => {
-      setIsAuthenticated(true);
-    })
-    .catch(err => {
-      if (err.response.status === 401) {
-        setIsAuthenticated(false);
-
-        axios.post('/user/logout')
-          .then(res => {
-          })
-          .catch(err => {
-            console.log(err);
-          })
-      }
-    })
 
   useEffect(() => {
+    console.log('hi');
+
     const script1 = document.createElement('script');
     script1.src = 'https://code.jquery.com/jquery-1.12.4.min.js';
     script1.async = true;
@@ -66,21 +48,45 @@ function App() {
     };
   }, [])
 
+  useEffect(() => {
+    // accessToken, refreshToken 검증
+    axios.get('/user/verify', { withCredentials: true })
+      .then(res => {
+        setIsAuthenticated(true);
+      })
+      .catch(err => {
+        if (err.response.status === 401) {
+          setIsAuthenticated(false);
+
+          axios.post('/user/logout')
+            .then(res => {
+            })
+            .catch(err => {
+              console.log(err);
+            })
+        }
+      })
+  }, [location]);
+
   return (
     <div id='App'>
       <GlobalStyle />
-      <BrowserRouter>
-        <Header isAuthenticated={isAuthenticated} />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/ticketing' element={<Ticketing />} />
-            <Route path='/user/*' element={<UserRouter />} />
-            <Route path='/movie/*' element={<MovieRouter />} />
-          </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Header isAuthenticated={isAuthenticated} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ticketing' element={<Ticketing />} />
+        <Route path='/user/*' element={<UserRouter />} />
+        <Route path='/movie/*' element={<MovieRouter />} />
+      </Routes>
+      <Footer />
     </div>
   )
 }
 
-export default App;
+export default function Root() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
